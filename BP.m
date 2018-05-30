369e419692e88b92b3b3c1060a85ce0dd8e5967a
NexturnZ <ZhouLKmail@gmail.com>
2018-05-30 10:44:39 +0800

    Version1.5: A correction of format of message matrix

--- BP.m
+++ BP.m
@@ -2,28 +2,32 @@ function BP(MRF, Vd, Vs)
 iteration = 20;         % iteration times
 counter = 0;            % counter for iteration
 s = size(MRF);          % obtain the size of image
-m = ones([s,25]);     % initialize message
+m = ones([s,4,25]);     % initialize message
 
 while(counter<=iteration)
     for i1 = 2:s(1)-1
        for i2 = 2:s(2)-1
            
-          if MRF(i1,i2)==1 && MRF(i1-1,i2)==1
-              m = mesCompute(m, Vd, Vs, node, 0);
-          end
-          
-          if MRF(i1,i2)==1 && MRF(i1,i2+1)==1
+          if MRF(i1,i2)==1 && MRF(i1-1,i2)==1           % pass message for 4 neighbor
               m = mesCompute(m, Vd, Vs, node, 1);
           end
           
-          if MRF(i1,i2)==1 && MRF(i1+1,i2)==1
+          if MRF(i1,i2)==1 && MRF(i1,i2+1)==1
               m = mesCompute(m, Vd, Vs, node, 2);
           end
           
-          if MRF(i1,i2)==1 && MRF(i1,i2-1)==1
+          if MRF(i1,i2)==1 && MRF(i1+1,i2)==1
               m = mesCompute(m, Vd, Vs, node, 3);
+          end
+          
+          if MRF(i1,i2)==1 && MRF(i1,i2-1)==1
+              m = mesCompute(m, Vd, Vs, node, 4);
           end
           
        end
     end
 end
+
+
+end
+
--- mesCompute.m
+++ mesCompute.m
@@ -5,7 +5,7 @@
 % Vs
 % node: index of node               --> 1x2 vector
 % dir: the position of another node --> scale
-%       0:up, 1:right, 2:down, 3:left
+%       1:up, 2:right, 3:down, 4:left
 %                     
 % Output:
 % m_new: new message matrix         --> MxNx25 matrix;
@@ -14,24 +14,24 @@ function m_new = mesCompute(m, Vd, Vs, node, dir)
 m_new = m;
 
 switch dir
-    case 0
-        m_pre = squeeze(m(node(1),node(2)+1));
-        m_pre = m_pre.*squeeze(m(node(1)+1,node(2)));
-        m_pre = m_pre.*squeeze(m(node(1),node(2)-1));
     case 1
-        m_pre = squeeze(m(node(1)-1,node(2)));
+        m_pre = squeeze(m(node(1),node(2)+1));
         m_pre = m_pre.*squeeze(m(node(1)+1,node(2)));
         m_pre = m_pre.*squeeze(m(node(1),node(2)-1));
     case 2
         m_pre = squeeze(m(node(1)-1,node(2)));
-        m_pre = m_pre.*squeeze(m(node(1),node(2)+1));
+        m_pre = m_pre.*squeeze(m(node(1)+1,node(2)));
         m_pre = m_pre.*squeeze(m(node(1),node(2)-1));
     case 3
+        m_pre = squeeze(m(node(1)-1,node(2)));
+        m_pre = m_pre.*squeeze(m(node(1),node(2)+1));
+        m_pre = m_pre.*squeeze(m(node(1),node(2)-1));
+    case 4
         m_pre = squeeze(m(node(1)-1,node(2)));
         m_pre = m_pre.*squeeze(m(node(1),node(2)+1));
         m_pre = m_pre.*squeeze(m(node(1)+1,node(2)));
 end
 for i1 = 1:25   % for everys
-    m_new(node(1),node(2),i1) = max(squeeze(Vd(node(1),node(2))).*Vs(:,i1).*m_pre);
+    m_new(node(1),node(2),dir,i1) = max(squeeze(Vd(node(1),node(2))).*Vs(:,i1).*m_pre);
 end
 end
