83edb9cc6401790dd93fdf8b7c0572081095d152
NexturnZ <ZhouLKmail@gmail.com>
2018-06-03 00:36:17 +0800

    Version 2.1: random select node in BP.

--- BP.m
+++ BP.m
@@ -1,51 +1,27 @@
 function alphaK_new = BP(MRF, Vd, Vs,alphaK, level)
-iteration = 30;         % iteration times
 s = size(MRF);          % obtain the size of image
 m = zeros([s,4,level]);     % initialize message
 m_new = ones([s,4,level]);
-counter = 1;
 
 x = 1:s(1); y = 1:s(2);
 [X,Y] = meshgrid(x,y);
 X = X.';    Y = Y.';
 
-while sum(m_new(:) == m(:))~=numel(m) & counter < iteration
+X_mrf = X(MRF==0);
+Y_mrf = Y(MRF==0);
+iteration = 20*length(X_mrf);         % iteration times
+counter = 1;
+% while sum(m_new(:) == m(:))~=numel(m) & counter < iteration
+while counter < iteration
     m = m_new;
-    
-    for i1 = 1:level
-        Vs_rep = ones(1,1,level);
-        Vs_rep(1,1,:)= Vs(i1,:);
-        Vs_rep = repmat(Vs_rep,s(1),s(2),1);
-
-        m_up = squeeze(prod(m(:,:,[2,3,4],:),3));    % MxNx25 matrix
-        m_up = max(Vd.*m_up.*Vs_rep,[],3);
-        m_new(1:s(1)-1,:,1,i1) = m_up(2:s(1),:);
-
-        m_right = squeeze(prod(m(:,:,[1,3,4],:),3));
-        m_right = max(Vd.*m_right.*Vs_rep,[],3);
-        m_new(:,2:s(2),2,i1) = m_right(:,1:s(2)-1);
-
-        m_down = squeeze(prod(m(:,:,[1,2,4],:),3));
-        m_down = max(Vd.*m_down.*Vs_rep,[],3);
-        m_new(2:s(1),:,3,i1) = m_down(1:s(1)-1,:);
-
-        m_left = squeeze(prod(m(:,:,[1,2,3],:),3));
-        m_left = max(Vd.*m_left.*Vs_rep,[],3);
-        m_new(:,1:s(2)-1,4,i1) = m_left(:,2:s(2));
-
-    end
-    
-    X_mrf = X(MRF==0);
-    Y_mrf = Y(MRF==0);
-    for i1 = 1:length(X_mrf)
-        m_new(X_mrf(i1),Y_mrf(i1),:,:)= ones(1,1,4,level);
-    end
-    
-    
-    fprintf('The %d iteration\n',counter);
+    idx = ceil(length(X_mrf)*rand(1));
+    node = [X_mrf(idx),Y_mrf(idx)];
+    m_new = mesCompute(m, Vd, Vs, node);
     counter = counter+1;
     
 end
+
+fprintf('%d iteration for BP algorithm\n',counter);
 
 % compute belief vector, b should be a 3D matrix
 b = Vd.*squeeze(sum(m_new,3));  
--- mesCompute.m
+++ mesCompute.m
@@ -10,28 +10,40 @@
 % Output:
 % m_new: new message matrix         --> MxNx25 matrix;
 %-------------------------------------------------------------------------%
-function m_new = mesCompute(m, Vd, Vs, node, dir)
+function m_new = mesCompute(m, Vd, Vs, node)
 m_new = m;
+s = size(Vd(:,:,1));
 
-switch dir
-    case 1          % up
-        m_pre = squeeze(m(node(1),node(2)+1,4,:));          % message from right node
-        m_pre = m_pre.*squeeze(m(node(1)+1,node(2),1,:));   % message from down node
-        m_pre = m_pre.*squeeze(m(node(1),node(2)-1,2,:));   % message from left node
-    case 2          % right
-        m_pre = squeeze(m(node(1)-1,node(2),3,:));          % message from up node
-        m_pre = m_pre.*squeeze(m(node(1)+1,node(2),1,:));   % message from down node
-        m_pre = m_pre.*squeeze(m(node(1),node(2)-1,2,:));   % message from left node
-    case 3          % down
-        m_pre = squeeze(m(node(1)-1,node(2),3,:));          % message from up node
-        m_pre = m_pre.*squeeze(m(node(1),node(2)+1,4,:));   % message from right node
-        m_pre = m_pre.*squeeze(m(node(1),node(2)-1,2,:));   % message from left node
-    case 4          % left
-        m_pre = squeeze(m(node(1)-1,node(2),3,:));          % message from up node           
-        m_pre = m_pre.*squeeze(m(node(1),node(2)+1,4,:));   % message from right node
-        m_pre = m_pre.*squeeze(m(node(1)+1,node(2),1,:));   % message from down node
+% up
+if node(1) ~= 1
+    m_up = squeeze(prod(m(node(1),node(2)+1,[2,3,4],:),3));
+    for i1 = 1:25   % for everys
+        m_new(node(1)-1,node(2),3,i1) = max(squeeze(Vd(node(1),node(2))).*Vs(:,i1).*m_up);
+    end
 end
-for i1 = 1:25   % for everys
-    m_new(node(1),node(2),dir,i1) = max(squeeze(Vd(node(1),node(2))).*Vs(:,i1).*m_pre);
+
+% right
+if node(2) ~= s(2)
+    m_right = squeeze(prod(m(node(1)-1,node(2),[1,3,4],:),3));          % message from up node
+    for i1 = 1:25   % for everys
+        m_new(node(1),node(2)+1,4,i1) = max(squeeze(Vd(node(1),node(2))).*Vs(:,i1).*m_right);
+    end
 end
+
+% down
+if node(1) ~= s(1)
+    m_down = squeeze(prod(m(node(1)-1,node(2),[1,2,4],:),3));          % message from up node
+    for i1 = 1:25   % for everys
+        m_new(node(1)+1,node(2),1,i1) = max(squeeze(Vd(node(1),node(2))).*Vs(:,i1).*m_down);
+    end
+end
+
+% left
+if node(2) ~= 1
+    m_left = squeeze(prod(m(node(1)-1,node(2),[1,2,3],:),3));          % message from up node      
+    for i1 = 1:25   % for everys
+        m_new(node(1),node(2)-1,2,i1) = max(squeeze(Vd(node(1),node(2))).*Vs(:,i1).*m_left);
+    end
+end
+
 end
