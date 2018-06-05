8c359cb4da2284f7c2fc63986ec4159bcc641379
NexturnZ <ZhouLKmail@gmail.com>
2018-06-05 10:26:36 +0800

    Version 1.22: further debugging, mainly about matrix size matching. Results seems better.

--- BP.m
+++ BP.m
@@ -19,7 +19,7 @@ Y_mrf = Y(MRF==1);
 X_n = X(MRF==0);            % pixels that are not in MRF
 Y_n = Y(MRF==0);
 
-b = zeros([s,level])
+b = zeros([s,level]);
 
 while sum(m_new(:) == m(:))~=numel(m) & counter < iteration
     m = m_new;
@@ -29,83 +29,102 @@ while sum(m_new(:) == m(:))~=numel(m) & counter < iteration
         Vs_corner(1,1,:)= Vs(i1,:);
         Vs_edge_hor = repmat(Vs_rep,1,s(2)-2,1);
         Vs_edge_ver = repmat(Vs_rep,s(1)-2,1,1);
-        Vs_cen = repmat(Vs_rep,s(1),s(2),1);
+        Vs_cen = repmat(Vs_rep,s(1)-2,s(2)-2,1);
 
         %% message for nodes at four corners
+        m_pre = ones(1,1,level);
         % left-up corner
-        m_new(1,1,2,i1) = max(Vd(1,1,:).*Vs_corner.* squeeze(m(2,1,1,:)),[],3);
-        m_new(1,1,3,i1) = max(Vd(1,1,:).*Vs_corner.* squeeze(m(1,2,1,:)),[],3);
+        m_pre(1,1,:) = squeeze(m(2,1,1,:));
+        m_new(1,1,2,i1) = max(Vd(1,1,:).*Vs_corner.* m_pre,[],3);
+        m_pre(1,1,:) = squeeze(m(1,2,1,:));
+        m_new(1,1,3,i1) = max(Vd(1,1,:).*Vs_corner.* m_pre,[],3);
         
         % right-up corner
-        m_new(1,s(2),4,i1) = max(Vd(1,s(2),:).*Vs_corner.* squeeze(m(2,s(2),1,:)),[],3);
-        m_new(1,s(2),3,i1) = max(Vd(1,s(2),:).*Vs_corner.* squeeze(m(1,s(2)-1,1,:)),[],3);
+        m_pre(1,1,:) = squeeze(m(2,s(2),1,:));
+        m_new(1,s(2),4,i1) = max(Vd(1,s(2),:).*Vs_corner.* m_pre,[],3);
+        m_pre(1,1,:) = squeeze(m(1,s(2)-1,1,:));
+        m_new(1,s(2),3,i1) = max(Vd(1,s(2),:).*Vs_corner.* m_pre,[],3);
         
         % left-down corner
-        m_new(s(1),1,1,i1) = max(Vd(s(1),1,:).*Vs_corner.* squeeze(m(s(1),2,1,:)),[],3);
-        m_new(s(1),1,2,i1) = max(Vd(s(1),1,:).*Vs_corner.* squeeze(m(s(1)-1,1,1,:)),[],3);
+        m_pre(1,1,:) = squeeze(m(s(1),2,1,:));
+        m_new(s(1),1,1,i1) = max(Vd(s(1),1,:).*Vs_corner.* m_pre,[],3);
+        m_pre(1,1,:) = squeeze(m(s(1)-1,1,1,:));
+        m_new(s(1),1,2,i1) = max(Vd(s(1),1,:).*Vs_corner.* m_pre,[],3);
         
         % right-down corner
-        m_new(s(1),s(2),1,i1) = max(Vd(s(1),s(2),:).*Vs_corner.* squeeze(m(s(1),s(2)-1,1,:)),[],3);
-        m_new(s(1),s(2),4,i1) = max(Vd(s(1),s(2),:).*Vs_corner.* squeeze(m(s(1)-1,s(2),1,:)),[],3);
+        m_pre(1,1,:) = squeeze(m(s(1),s(2)-1,1,:));
+        m_new(s(1),s(2),1,i1) = max(Vd(s(1),s(2),:).*Vs_corner.* m_pre,[],3);
+        m_pre(1,1,:) = squeeze(m(s(1)-1,s(2),1,:));
+        m_new(s(1),s(2),4,i1) = max(Vd(s(1),s(2),:).*Vs_corner.* m_pre,[],3);
         
         
         
         %% message for nodes at four edges of image
+        
         % up edge
-        m_right = squeeze(m(1,1:s(2)-2,2,:).*m(2,2:s(2)-1,1,:));
-        m_new(1,2:s(2)-1,2,i1) = max(m_right.*Vd(1,2:s(2)-1,:).*Vs_edge_hor,[],3);
+        m_pre = zeros(1,s(2)-2,level);
+        m_pre(1,:,:) = squeeze(m(1,1:s(2)-2,2,:).*m(2,2:s(2)-1,1,:));
+        m_new(1,2:s(2)-1,2,i1) = max(m_pre.*Vd(1,2:s(2)-1,:).*Vs_edge_hor,[],3);
         
-        m_down = squeeze(m(1,1:s(2)-2,2,:).*m(1,3:s(2),4,:));
-        m_new(1,2:s(2)-1,3,i1) = max(m_down.*Vd(1,2:s(2)-1,:).*Vs_edge_hor,[],3);
+        m_pre(1,:,:) = squeeze(m(1,1:s(2)-2,2,:).*m(1,3:s(2),4,:));
+        m_new(1,2:s(2)-1,3,i1) = max(m_pre.*Vd(1,2:s(2)-1,:).*Vs_edge_hor,[],3);
         
-        m_left = squeeze(m(2,2:s(2)-1,1,:).*m(1,3:s(2),4,:));
-        m_new(1,2:s(2)-1,4,i1) = max(m_left.*Vd(1,2:s(2)-1,:).*Vs_edge_hor,[],3);
+        m_pre(1,:,:) = squeeze(m(2,2:s(2)-1,1,:).*m(1,3:s(2),4,:));
+        m_new(1,2:s(2)-1,4,i1) = max(m_pre.*Vd(1,2:s(2)-1,:).*Vs_edge_hor,[],3);
         
         
         % right edge
-        m_up = squeeze(m(3:s(1),s(2),1,:).*m(2:s(1)-1,s(2)-1,2,:));
-        m_new(2:s(1)-1,s(2),1,i1) = max(m_up.*Vd(2:s(1)-1,s(2),:).*Vs_edge_ver,[],3);
+        m_pre = zeros(s(1)-2,1,level);
+        m_pre(:,1,:) = squeeze(m(3:s(1),s(2),1,:).*m(2:s(1)-1,s(2)-1,2,:));
+        m_new(2:s(1)-1,s(2),1,i1) = max(m_pre.*Vd(2:s(1)-1,s(2),:).*Vs_edge_ver,[],3);
         
-        m_down = squeeze(m(1:s(1)-2,s(2),3,:).*m(2:s(1)-1,s(2)-1,2,:));
-        m_new(2:s(1)-1,s(2),3,i1) = max(m_down.*Vd(2:s(1)-1,s(2),:).*Vs_edge_ver,[],3);
+        m_new(2:s(1)-1,s(2),3,i1) = max(m_pre.*Vd(2:s(1)-1,s(2),:).*Vs_edge_ver,[],3);
         
-        m_left = squeeze(m(1:s(1)-2,s(2),3,:).*m(3:s(1),s(2),2,:));
-        m_new(2:s(1)-1,s(2),4,i1) = max(m_left.*Vd(2:s(1)-1,s(2),:).*Vs_edge_ver,[],3);
+        m_pre(:,1,:) = squeeze(m(1:s(1)-2,s(2),3,:).*m(3:s(1),s(2),2,:));
+        m_new(2:s(1)-1,s(2),4,i1) = max(m_pre.*Vd(2:s(1)-1,s(2),:).*Vs_edge_ver,[],3);
         
         
         % down edge
-        m_up = squeeze(m(s(1),1:s(2)-2,2,:).*m(s(1),3:s(2),4,:));
-        m_new(s(1),2:s(2)-1,1,i1) = max(m_up.*Vd(s(1),2:s(2)-1,:).*Vs_edge_hor,[],3);
+        m_pre = zeros(1,s(2)-2,level);
+        m_pre(1,:,:) = squeeze(m(s(1),1:s(2)-2,2,:).*m(s(1),3:s(2),4,:));
+        m_new(s(1),2:s(2)-1,1,i1) = max(m_pre.*Vd(s(1),2:s(2)-1,:).*Vs_edge_hor,[],3);
         
-        m_right = squeeze(m(s(1)-1,2:s(2)-1,3,:).*m(s(1),1:s(2)-2,2,:));
-        m_new(s(1),2:s(2)-1,2,i1) = max(m_right.*Vd(s(1),2:s(2)-1,:).*Vs_edge_hor,[],3);
+        m_pre(1,:,:) = squeeze(m(s(1)-1,2:s(2)-1,3,:).*m(s(1),1:s(2)-2,2,:));
+        m_new(s(1),2:s(2)-1,2,i1) = max(m_pre.*Vd(s(1),2:s(2)-1,:).*Vs_edge_hor,[],3);
         
-        m_left = squeeze(m(s(1)-1,2:s(2)-1,3,:).*m(s(1),3:s(2),4,:));
-        m_new(s(1),2:s(2)-1,4,i1) = max(m_left.*Vd(s(1),2:s(2)-1,:).*Vs_edge_hor,[],3);
+        m_pre(1,:,:) = squeeze(m(s(1)-1,2:s(2)-1,3,:).*m(s(1),3:s(2),4,:));
+        m_new(s(1),2:s(2)-1,4,i1) = max(m_pre.*Vd(s(1),2:s(2)-1,:).*Vs_edge_hor,[],3);
         
         
         % left edge
-        m_up = squeeze(m(2:s(1)-1,2,4,:).*m(3:s(1),1,1,:));
-        m_new(2:s(1)-1,1,1,i1) = max(m_up.*Vd(2:s(1)-1,1,:).*Vs_edge_ver,[],3);
+        m_pre = zeros(s(1)-2,1,level);
+        m_pre(:,1,:) = squeeze(m(2:s(1)-1,2,4,:).*m(3:s(1),1,1,:));
+        m_new(2:s(1)-1,1,1,i1) = max(m_pre.*Vd(2:s(1)-1,1,:).*Vs_edge_ver,[],3);
         
-        m_right = squeeze(m(1:s(1)-2,1,3,:).*m(3:s(1),1,1,:));
-        m_new(2:s(1)-1,1,2,i1) = max(m_right.*Vd(2:s(1)-1,1,:).*Vs_edge_ver,[],3);
+        m_pre(:,1,:) = squeeze(m(1:s(1)-2,1,3,:).*m(3:s(1),1,1,:));
+        m_new(2:s(1)-1,1,2,i1) = max(m_pre.*Vd(2:s(1)-1,1,:).*Vs_edge_ver,[],3);
         
-        m_down = squeeze(m(1:s(1)-2,1,3,:).*m(2:s(1)-1,2,1,:));
-        m_new(2:s(1)-1,1,3,i1) = max(m_down.*Vd(2:s(1)-1,1,:).*Vs_edge_ver,[],3);
+        m_pre(:,1,:) = squeeze(m(1:s(1)-2,1,3,:).*m(2:s(1)-1,2,1,:));
+        m_new(2:s(1)-1,1,3,i1) = max(m_pre.*Vd(2:s(1)-1,1,:).*Vs_edge_ver,[],3);
+        
+        
         
         %% message for nodes at center
-        m_up = squeeze(m(2:s(1)-1,3:s(2),4,:).*m(3:s(1),2:s(2)-1,1,:).*m(2:s(1)-1,1:s(2)-1,2,:));
-        m_new(2:s(1)-1,2:s(2)-1,1,:) = max(m_up.*Vd(2:s(1)-1,2:s(2)-1,:).*Vs_cen,[],3);
+%         m_up = zeros([s-2,1,level]);
+        m_up = squeeze(m(2:s(1)-1,3:s(2),4,:).*m(3:s(1),2:s(2)-1,1,:).*m(2:s(1)-1,1:s(2)-2,2,:));
+        m_new(2:s(1)-1,2:s(2)-1,1,i1) = max(m_up.*Vd(2:s(1)-1,2:s(2)-1,:).*Vs_cen,[],3);
         
-        m_right = squeeze(m(1:s(1)-2,2:s(2)-1,3,:).*m(3:s(1),2:s(2)-1,1,:).*m(2:s(1)-1,1:s(2)-1,2,:));
-        m_new(2:s(1)-1,2:s(2)-1,2,:) = max(m_right.*Vd(2:s(1)-1,2:s(2)-1,:).*Vs_cen,[],3);
+%         m_right = zeros([s,1,level]);
+        m_right = squeeze(m(1:s(1)-2,2:s(2)-1,3,:).*m(3:s(1),2:s(2)-1,1,:).*m(2:s(1)-1,1:s(2)-2,2,:));
+        m_new(2:s(1)-1,2:s(2)-1,2,i1) = max(m_right.*Vd(2:s(1)-1,2:s(2)-1,:).*Vs_cen,[],3);
         
-        m_down = squeeze(m(1:s(1)-2,2:s(2)-1,3,:).*m(2:s(1)-1,3:s(2),4,:).*m(2:s(1)-1,1:s(2)-1,2,:));
-        m_new(2:s(1)-1,2:s(2)-1,2,:) = max(m_down.*Vd(2:s(1)-1,2:s(2)-1,:).*Vs_cen,[],3);
+%         m_down = zeros([s,1,level]);
+        m_down = squeeze(m(1:s(1)-2,2:s(2)-1,3,:).*m(2:s(1)-1,3:s(2),4,:).*m(2:s(1)-1,1:s(2)-2,2,:));
+        m_new(2:s(1)-1,2:s(2)-1,2,i1) = max(m_down.*Vd(2:s(1)-1,2:s(2)-1,:).*Vs_cen,[],3);
         
+%         m_left = zeros([s,1,level]);
         m_left = squeeze(m(1:s(1)-2,2:s(2)-1,3,:).*m(2:s(1)-1,3:s(2),4,:).*m(3:s(1),2:s(2)-1,1,:));
-        m_new(2:s(1)-1,2:s(2)-1,2,:) = max(m_left.*Vd(2:s(1)-1,2:s(2)-1,:).*Vs_cen,[],3);
+        m_new(2:s(1)-1,2:s(2)-1,2,i1) = max(m_left.*Vd(2:s(1)-1,2:s(2)-1,:).*Vs_cen,[],3);
         
     end
     
@@ -125,20 +144,45 @@ while sum(m_new(:) == m(:))~=numel(m) & counter < iteration
     
 end
 
-% compute belief vector, b should be a 3D matrix
+for i1 = 1:length(X_n)
+    m_new(X_n(i1),Y_n(i1),:,:)= zeros(1,1,4,level);
+end
+
+%% compute belief vector, b should be a 3D matrix
 % corner nodes
-b(1,1,:) = Vd(1,1,:).*squeeze(m(1,2,4,:)+m(2,1,1,:));
-b(1,s(1),:) = Vd(1,s(1),:).*squeeze(m(2,s(1),1,:)+m(1,s(1)-1,2,:));
-b(s(1),s(2),:) = Vd(s(1),s(2),:).*squeeze(m(s(1)-1,s(2),3,:)+m(s(1),s(2)-1,2,:));
-b(s(1),1,:) = Vd(s(1),1,:).*squeeze(m(s(1)-1,1,3,:)+m(s(1),2,4,:));
+m_sum = zeros(1,1,level);
+
+m_sum(1,1,:) = squeeze(m(1,2,4,:)+m(2,1,1,:));
+b(1,1,:) = Vd(1,1,:).*m_sum;
+
+m_sum(1,1,:) = squeeze(m(2,s(1),1,:)+m(1,s(1)-1,2,:));
+b(1,s(1),:) = Vd(1,s(1),:).*m_sum;
+
+m_sum(1,1,:) = squeeze(m(s(1)-1,s(2),3,:)+m(s(1),s(2)-1,2,:));
+b(s(1),s(2),:) = Vd(s(1),s(2),:).*m_sum;
+
+m_sum(1,1,:) = squeeze(m(s(1)-1,1,3,:)+m(s(1),2,4,:));
+b(s(1),1,:) = Vd(s(1),1,:).*m_sum;
+
 % edge nodes
-b(1,2:s(2)-1,:) = Vd(1,2:s(2)-1,:).*squeeze(m(1,3:s(2),4,:)+m(2,2:s(1)-1,1,:)+m(1,1:s(2)-2,2,:));
-b(2:s(1)-1,s(2),:) = Vd(2:s(1)-1,s(2),:).*squeeze(m(1:s(1)-2,s(2),3,:)+m(3:s(1),s(2),1,:)+m(2:s(1)-1,s(2)-1,2,:));
-b(s(1),2:s(2)-1,:) = Vd(s(1),2:s(2)-1,:).*squeeze(m(s(1)-1,2:s(2)-1,3,:)+m(s(1),3:s(2),4,:)+m(s(1),1:s(2)-2,2,:));
-b(2:s(1)-1,1,:) = Vd(2:s(1)-1,1,:).*squeeze(m(1:s(1)-2,1,3,:)+m(2:s(2)-1,2,4,:)+m(3:s(1),1,1,:));
+m_sum = zeros(1,s(2)-2,level);
+m_sum(1,:,:) = squeeze(m(1,3:s(2),4,:)+m(2,2:s(2)-1,1,:)+m(1,1:s(2)-2,2,:));
+b(1,2:s(2)-1,:) = Vd(1,2:s(2)-1,:).*m_sum;
+
+m_sum = zeros(s(1)-2,1,level);
+m_sum(:,1,:) = squeeze(m(1:s(1)-2,s(2),3,:)+m(3:s(1),s(2),1,:)+m(2:s(1)-1,s(2)-1,2,:));
+b(2:s(1)-1,s(2),:) = Vd(2:s(1)-1,s(2),:).*m_sum;
+
+m_sum = zeros(1,s(2)-2,level);
+m_sum(1,:,:) = squeeze(m(s(1)-1,2:s(2)-1,3,:)+m(s(1),3:s(2),4,:)+m(s(1),1:s(2)-2,2,:));
+b(s(1),2:s(2)-1,:) = Vd(s(1),2:s(2)-1,:).*m_sum;
+
+m_sum = zeros(s(1)-2,1,level);
+m_sum(:,1,:) = squeeze(m(1:s(1)-2,1,3,:)+m(2:s(1)-1,2,4,:)+m(3:s(1),1,1,:));
+b(2:s(1)-1,1,:) = Vd(2:s(1)-1,1,:).*m_sum;
 % center nodes
 b(2:s(1)-1,2:s(2)-1,:) = Vd(2:s(1)-1,2:s(2)-1,:).*...
-    squeeze(m(1:s(1)-2,2:s(2)-1,3,:)+m(2:s(1)-2,3:s(2),4,:)+m(3:s(1),2:s(2)-1,1,:)+m(2:s(1)-1,1:s(2)-2,2,:));
+    squeeze(m(1:s(1)-2,2:s(2)-1,3,:)+m(2:s(1)-1,3:s(2),4,:)+m(3:s(1),2:s(2)-1,1,:)+m(2:s(1)-1,1:s(2)-2,2,:));
 
 
 % compute alpha level, alphaK_level should be a 2D matrix
